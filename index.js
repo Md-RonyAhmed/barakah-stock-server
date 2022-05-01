ca60b83ff3c91b7cbb2024f6e1e8c3247f19e2c8
Md. Rony <rony15-10527@diu.edu.bd>
2022-05-02 01:29:00 +0600

    local server connected

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+
+
+
+
+
+
+// testing api 
+app.get("/json", (req, res) => {
+   res.send({ message: "Barakah stock running"});
+});
+
+app.listen(port, () => {
+   console.log(`Server running on port: ${port}`)
+});
