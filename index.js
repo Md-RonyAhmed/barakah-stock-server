74b617a0ea20ca5157f912f3cdb15901fd048d52
Md. Rony <rony15-10527@diu.edu.bd>
2022-05-02 02:08:09 +0600

    DB connected to mongoDB

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const express = require('express');
+const cors = require("cors");
 const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
+// DB connection
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@barakah.z3u4t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+  
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
+(async () => {
+   try {
+      await client.connect();
+      const productCollection = client.db('barakah').collection('products');
+      console.log('DB connected');
+   } catch (error) {
+      console.log(error);
+   }
+})();
+
+// middleware
+app.use(cors());
 
 
 
