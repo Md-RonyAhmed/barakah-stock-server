0369570ae6ccc3cdc0335e2846056c2e832798ef
Md. Rony <rony15-10527@diu.edu.bd>
2022-05-07 23:37:03 +0600

    get api completed with limit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { query } = require("express");
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -11,7 +12,7 @@ app.use(express.json());
 
 // DB connection
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@barakah.z3u4t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-  
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -19,47 +20,56 @@ const client = new MongoClient(uri, {
 });
 
 (async () => {
-   try {
-      await client.connect();
-      const productCollection = client.db('barakah').collection('products');
-      // console.log('DB connected');
-      // post data to DB 
-      app.post('/products', async(req, res) => {
-         const product = req.body;
-         await productCollection.insertOne(product);
-         // res.json({ product });
-         res.send({ success: true, message: `Successfully inserted ${product.name}` });
-      })
-      // get all products 
-      app.get('/products', async(req, res) => {
-         const query = {};
-         const cursor = productCollection.find(query);
-         const products = await cursor.toArray();
-         res.send(products);
-      })
-      // delete 
-      app.delete('/products/:id', async(req, res) => {
-         const id = req.params.id;
-         const query = { _id: ObjectId(id) };
-         const result = await productCollection.deleteOne(query);
-         // res.send(result);
-         res.send({
-           success: true,
-           message: `Successfully deleted`,
-         });
-      })
-   } catch (error) {
-      console.log(error);
-   }
+  try {
+    await client.connect();
+    const productCollection = client.db("barakah").collection("products");
+    // console.log('DB connected');
+    // post data to DB
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      await productCollection.insertOne(product);
+      // res.json({ product });
+      res.send({
+        success: true,
+        message: `Successfully inserted ${product.name}`,
+      });
+    });
+    // get all products
+    app.get("/products", async (req, res) => {
+       const limit = Number(req.query.limit);
+       console.log(limit);
+      const cursor = productCollection.find();
+       const products = await cursor.limit(limit).toArray();
+       console.log(products);
+      if (!products?.length) {
+        return res.send({ success: false, error: "No product found" });
+      }
+      res.send({
+        success: true,
+        data: products
+      });
+    });
+    // delete
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      // res.send(result);
+      res.send({
+        success: true,
+        message: `Successfully deleted`,
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
 })();
 
-
-
-// testing api 
+// testing api
 app.get("/json", (req, res) => {
-   res.send({ message: "Barakah stocks running"});
+  res.send({ message: "Barakah stocks running" });
 });
 
 app.listen(port, () => {
-   console.log(`Server running on port: ${port}`)
+  console.log(`Server running on port: ${port}`);
 });
